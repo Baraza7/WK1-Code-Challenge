1d590b688ba3b0c81c7cd89ee28111674397e46d
baraza7 <barazabrian87@gmail.com>
2022-11-28 20:24:52 +0300

    Pushing all 3 assignments but challange 3 is failing to parse the input as an integer, which is required to compute the output. If we hard code the values the code works but if we prompt for an input there is no output

---
+++ challangewk1_1/codeChallangewk1_1.js
@@ -0,0 +1,36 @@
+//Challenge 1: Student Grade Generator (Toy Problem)
+/*Write that prompts the user to input student marks. The input should be between 0 and 100. 
+Then output the correct grade: 
+A > 79, B - 60 to 79, C -  59 to 49, D - 40 to 49, E - less 40.*/
+
+
+
+let studentMark = 0;
+    
+studentMark= window.prompt ("Input your marks");
+
+
+switch(true)
+    {
+         case studentMark < 40:
+            document.write("Grade E");
+            break;
+            
+        case studentMark < 49 && studentMark >= 59:
+            document.write("Grade D");
+            break;   
+        
+        case studentMark < 79 && studentMark >= 60:
+            document.write("Grade C");
+            break;
+            
+        case grade < 100 && grade >= 79:
+            document.write("Grade A");
+            break;
+            
+            
+         default:
+            document.write("Mark Not Valid"); 
+    }
+
+    
---
+++ challangewk1_2/codeChallangewk1_2.js
@@ -0,0 +1,19 @@
+//Challenge 2: Speed Detector (Toy Problem)
+/*Write a program that takes as input the speed of a car e.g 80. 
+If the speed is less than 70, it should print “Ok”. Otherwise, 
+for every 5 km/s above the speed limit (70), it should give the
+driver one demerit point and print the total number of demerit points.*/
+
+
+
+let speed = 0;
+    
+speed= window.prompt ("What is your speed");
+
+
+    if (speed < 70) {
+        document.write("ok");
+    } 
+      else {
+        document.write("Your total demerit points is: " + ((speed-70)/5) + " Points");
+      }
